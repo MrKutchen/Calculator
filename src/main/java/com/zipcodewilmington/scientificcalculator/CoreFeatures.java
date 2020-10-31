cc0837a6a59e42c0d388100d539aa743a9836c4e
Peter <pgkutc@gmail.com>
2020-10-31 17:52:28 -0400

    Building project

--- src/main/java/com/zipcodewilmington/scientificcalculator/CoreFeatures.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CoreFeatures.java
@@ -3,17 +3,18 @@ package com.zipcodewilmington.scientificcalculator;
 import java.util.Scanner;
 
 public class CoreFeatures {
-    private static int display = 0;
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void displayOptions() {
         boolean powerOn = true;
         while(powerOn) {
-            System.out.print("Please choose one of the following options:\n" +
+            System.out.print("\n" +
+                    "Please choose one of the following options:\n" +
                     "Type 1 for current display.\n" +
                     "Type 2 to clear display.\n" +
-                    "Type 3 to exit.\n" +
+                    "Type 3 to change the display value\n" +
+                    "Type 4 to go back to the Main Menu.\n" + "\n" +
                     "Enter choice here: ");
 
             int input = scanner.nextInt();
@@ -22,20 +23,30 @@ public class CoreFeatures {
                     break;
                 case 2: CoreFeatures.clearDisplay();
                     break;
-                case 3: powerOn = false;
+                case 3: CoreFeatures.changeDisplay();
+                break;
+                case 4: powerOn = false;
+                break;
             }
         }
     }
 
     public static void currentDisplay() {
         System.out.println("\n" +
-                "Display = " + display + "\n");
+                "Display = " + MainApplication.display);
     }
 
     public static void clearDisplay() {
-        display = 0;
+        MainApplication.display = 0;
         System.out.println("\n" +
-                "Display has been cleared. Display = " + display + "\n");
+                "Display has been cleared. Display = " + MainApplication.display);
+    }
+
+    public static void changeDisplay() {
+        System.out.println("\n" +
+                "Please choose what you would like the display integer to be?");
+        MainApplication.display = scanner.nextInt();
+        currentDisplay();
     }
 }
 
